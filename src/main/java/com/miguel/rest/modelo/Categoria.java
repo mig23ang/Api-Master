b3848d6ba6a66b478b0ad30a205e20cc0ce2b038
miguel angel reyes <mareyes46@misena.edu.co>
2023-03-25 14:59:57 -0500

    sin carga de datos en base H2

--- src/main/java/com/miguel/rest/modelo/Categoria.java
+++ src/main/java/com/miguel/rest/modelo/Categoria.java
@@ -9,8 +9,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @NoArgsConstructor @AllArgsConstructor
 @Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Categoria {
 
 	@Id @GeneratedValue
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,22 @@
--- Crear tabla categoria
+DROP TABLE IF EXISTS producto;
+DROP TABLE IF EXISTS categoria;
+
 CREATE TABLE categoria (
-  id BIGINT PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL
+    id BIGINT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
 );
 
--- Insertar datos en tabla categoria
 INSERT INTO categoria (id, nombre)
-VALUES 
-  (1, 'Categoría 1'),
-  (2, 'Categoría 2'),
-  (3, 'Categoría 3');
+VALUES (1, 'pescados'), (2, 'mariscos'), (3, 'carnes');
 
--- Crear tabla producto
 CREATE TABLE producto (
-  id BIGINT PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  descripcion VARCHAR(255),
-  precio DECIMAL(10,2) NOT NULL,
-  categoria_id BIGINT NOT NULL,
-  FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+    id BIGINT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255),
+    precio DECIMAL(10,2) NOT NULL,
+    categoria_id BIGINT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
 );
 
--- Insertar datos en tabla producto
 INSERT INTO producto (id, nombre, descripcion, precio, categoria_id)
-VALUES 
-  (1, 'Producto 1', 'Descripción del producto 1', 10.99, 1),
-  (2, 'Producto 2', 'Descripción del producto 2', 25.99, 2),
-  (3, 'Producto 3', 'Descripción del producto 3', 12.50, 1),
-  (4, 'Producto 4', 'Descripción del producto 4', 5.99, 3);
+VALUES (1, 'mojarra', 'Mojarra de rio', 10.99, 1), (2, 'calamar', 'de mar', 25.99, 2), (3, 'pierna de res', 'Res', 12.50, 3), (4, 'pollo semi criollo', NULL, 5.99, 3);
