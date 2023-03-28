7b3ebc5e65e0f5efbdf1e68853af0ea3bcab00b0
miguel angel reyes <mareyes46@misena.edu.co>
2023-03-28 15:42:27 -0500

    fix apidocs

--- src/main/java/com/miguel/rest/controller/ProductoController.java
+++ src/main/java/com/miguel/rest/controller/ProductoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,6 +50,7 @@ public class ProductoController {
 	 * 
 	 * @return
 	 */
+
 	@GetMapping("/producto/all")
 	public ResponseEntity<List<ProductoDTO>> obtenerTodos() {
 		List<Producto> productos = productoRepositorio.findAll();
