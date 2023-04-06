a346bb4ca5f76751fcab3f071acf52c9eda9dc00
miguel angel reyes <mareyes46@misena.edu.co>
2023-04-06 15:03:37 -0500

    agregado la paginacion empiezo a cambiar controlle

--- src/main/java/com/miguel/rest/controller/ProductoController.java
+++ src/main/java/com/miguel/rest/controller/ProductoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/miguel/rest/repos/CategoriaRepositorio.java
+++ src/main/java/com/miguel/rest/repos/CategoriaRepositorio.java
@@ -2,6 +2,7 @@ package com.miguel.rest.repos;
 
 import java.util.Optional;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -9,6 +10,11 @@ import com.miguel.rest.modelo.Categoria;
 
 public interface CategoriaRepositorio extends JpaRepository<Categoria, Long> {
 
-    //método para buscar una categoria por su nombre
+    // método para buscar una categoria por su nombre
     Optional<Categoria> findByNombre(@Param("nombre") String nombre);
+
+    // agregar consulta @query para buscar una categoria por su nombre, esto para:
+    // agregar consultas personalizadas
+    // @Query("SELECT c FROM Categoria c WHERE c.nombre = :nombre")
+    // Optional<Categoria> findByNombreMySql(@Param("nombre") String nombre);
 }
