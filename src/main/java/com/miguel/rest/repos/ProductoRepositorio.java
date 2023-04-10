87e97e4fad9459b8876bee6047dedf2ffa366d29
miguel angel reyes <mareyes46@misena.edu.co>
2023-04-10 14:21:41 -0500

    agregando JpaSpecificationExecutor

--- src/main/java/com/miguel/rest/repos/ProductoRepositorio.java
+++ src/main/java/com/miguel/rest/repos/ProductoRepositorio.java
@@ -3,10 +3,11 @@ package com.miguel.rest.repos;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.miguel.rest.modelo.Producto;
 
-public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
+public interface ProductoRepositorio extends JpaRepository<Producto, Long>, JpaSpecificationExecutor<Producto> {
 
     // método para buscar por nombre
     Page<Producto> findByNombreContainsIgnoreCase(String txt, Pageable pageable);
